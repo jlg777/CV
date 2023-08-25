6c786f1fded57a0654465381516bda936eea7a64
jlg777 <proyectojlg777@gmail.com>
2023-08-25 16:06:59 -0300

    Se agregaron modales a DATOS PERSONALES

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const showFamilia = document.querySelector("#show-familia");
+showFamilia.addEventListener("click", function () {
+  const alertDialog = document.querySelector("#alert-familia");
+  alertDialog.showModal();
+});
+const showUbicacion = document.querySelector("#show-ubicacion");
+showUbicacion.addEventListener("click", function () {
+  const alertDialog = document.querySelector("#alert-ubicacion");
+  alertDialog.showModal();
+});
+const showMobilidad1 = document.querySelector("#show-mobilidad1");
+showMobilidad1.addEventListener("click", function () {
+  const alertDialog = document.querySelector("#alert-mobilidad1");
+  alertDialog.showModal();
+});
+const showMobilidad2 = document.querySelector("#show-mobilidad2");
+showMobilidad2.addEventListener("click", function () {
+  const alertDialog = document.querySelector("#alert-mobilidad2");
+  alertDialog.showModal();
+});
+const showActividades = document.querySelector("#show-actividades");
+showActividades.addEventListener("click", function () {
+  const alertDialog = document.querySelector("#alert-actividades");
+  alertDialog.showModal();
+});
+
+
